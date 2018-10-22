b1d02886ba5ffe78f5a95022625e41e51518d449
RafaelPedruzzi <rafaelbelmockpedruzzi@hotmail.com>
2018-10-22 17:00:31 +0100

    Worst fit and decreasing worst fit working

--- src/main.c
+++ src/main.c
@@ -65,5 +65,5 @@ int compare(const void *a, const void *b)
 {
     const int *ia = (const int *)a; // casting pointer types 
     const int *ib = (const int *)b;
-    return *ia  - *ib; 
+    return *ib  - *ia; 
 }
